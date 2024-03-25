00ee9fb65a8ba292d35785c4f0a3ec56eab26837
XiandaDu <283149519du@gmail.com>
2024-03-25 11:04:41 -0400

    feat: google auth finished

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,17 +2,33 @@ const express = require('express')
 const router = express.Router()
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
+const User = require('../models/User')
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: process.env.GOOGLE_CALLBACK_URL
   },
-  function(accessToken, refreshToken, profile, cb) {
-    console.log(profile)
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
+ async function(accessToken, refreshToken, profile, done) {
+    const newUser = {
+        googleId: profile.id,
+        displayName: profile.displayName,
+        firstName: profile.name.givenName,
+        lastName: profile.name.familyName,
+        profileImage: profile.photos[0].value
+    }
+
+    try{
+        let user = await User.findOne({googleId: profile.id})
+        if(user){
+            done(null, user)
+        } else {
+            user = await User.create(newUser)
+            done(null, user)
+        }
+    }catch(e){
+        console.log(e)
+    }
   }
 ));
 
@@ -33,5 +49,20 @@ router.get('/google/callback',
 router.get('/login-failure', (req, res)=>{
     res.send('Something went wrong')
 })
+
+//Persist user data
+passport.serializeUser(function(user, done){
+    done(null, user.id);
+})
+
+//Retrieve user data from session
+passport.deserializeUser(async function(id, done){
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch (error) {
+        done(error, null);
+    }
+});
 
 module.exports = router

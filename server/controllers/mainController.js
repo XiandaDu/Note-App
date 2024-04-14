87bc81dc8746b84be9591067acd3bc5f81f09304
XiandaDu <283149519du@gmail.com>
2024-04-14 18:30:06 -0400

    feat: add FAQs and About and Features

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -14,3 +14,37 @@ exports.about = async(req, res) => {
     }
     res.render('about', locals)
 }
+
+exports.feature = async(req, res) => {
+    const locals = {
+        title: "Features of this Note App",
+        description: [
+            "1. User registration and login functionality",
+            "2. Create, edit, and delete notes",
+            "3. Ability to search notes",
+            "4. Note sharing functionality, such as sharing notes with other users"
+        ]
+    }
+    res.render('feature', locals)
+}
+
+exports.faqs = async(req, res) => {
+    const locals = {
+        title: "Frequently Asked Questions",
+        faqs: [
+            {
+              "question": "How do I register for an account?",
+              "answer": "To register for an account, click on the 'Sign Up' link on the homepage and it will instantly guide you to the Google login page."
+            },
+            {
+              "question": "How do I search for specific notes?",
+              "answer": "You can search for specific notes using the search bar located at the top of the app. Simply enter keywords or phrases related to the note you're looking for, and the app will display the title of relevant results."
+            },       
+            {
+                "question": "Is there a way to recover deleted notes?",
+                "answer": "No, once the note is deleted. It can no longer be recovered. "
+            },
+        ]
+    }
+    res.render('faqs', locals)
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,5 +5,7 @@ const mainController = require('../controllers/mainController')
 //App Route
 router.get('/', mainController.homepage)
 router.get('/about', mainController.about)
+router.get('/feature', mainController.feature)
+router.get('/faqs', mainController.faqs)
 
 module.exports = router
